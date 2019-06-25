38c6b4a8560e1e9bdb46716890160640032b66a1
MrMrRobat <appkiller16@gmail.com>
2019-06-25 21:18:40 +0300

    Removed unnecessary code from setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,6 @@ import re
 
 from setuptools import setup, find_packages
 
-try:
-    from pip.req import parse_requirements
-except ImportError:  # pip >= 10.0.0
-    from pip._internal.req import parse_requirements
-
 WORK_DIR = pathlib.Path(__file__).parent
 PACKAGE_DIR = WORK_DIR / 'anystrenum'
 
